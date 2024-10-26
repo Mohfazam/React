0ec0b1a03e998a4412d4c7c8f7b2abb3f210a011
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-26 16:17:14 +0530

    Horizontally Aligned

--- First_React_Project/src/App.jsx
+++ First_React_Project/src/App.jsx
@@ -6,12 +6,11 @@ import UserCard from '../src/components/usercard';
 
 function App() {
  
-
   return (
     <div className='container' >
-    <UserCard />
-    <UserCard />
-    <UserCard />
+    <UserCard name="sarwar1" desc="sarwar1"/>
+    <UserCard name="sarwar2" desc="sarwar2"/>
+    <UserCard name="sarwar3" desc="sarwar3"/>
 </div>
 
   )
--- First_React_Project/src/components/usercard.jsx
+++ First_React_Project/src/components/usercard.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import sarwar_pic from '../assets/a.png';
 import './usercard.css'
 
-const UserCard = () => {
+const UserCard = (props) => {
   return (
     <div className='user-container'>
-      <p id='user-name'>Sarwar Khan</p>
+      <p id='user-name'>{props.name}</p>
       <img id='user-img' src={sarwar_pic} alt="Sarwar JPG"></img>
-      <p id='user-desc'>Description Of sarwar</p>
+      <p id='user-desc'>Description Of {props.desc}</p>
     </div>
   )
 }
