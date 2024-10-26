062ff6a91bb421b271f5753a574dc69671aac1d6
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-27 02:26:43 +0530

    Buggy clock

--- ReactTest/src/App.jsx
+++ ReactTest/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
 function Counter() {
 
   const [count, setcount] = useState(0);
+  const [count2, setcount2] = useState(0);
 
   // let count  = 0;
   function increasecount(){
@@ -31,12 +32,18 @@ function Counter() {
     setcount(0);
   }
 
+  setInterval(function clock(){
+    setcount2(count2+1);
+  }, 1000);
+
   return (
     <div>
       <h1>{count}</h1>
     <button onClick={increasecount}>Increase Count</button>
     <button onClick={decreasecount}>Decrease Count</button>
     <button onClick={resetcount}>Reset Count</button>
+
+    <h1>Clock: {count2}</h1>
     </div>
   );
 }
