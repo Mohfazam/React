893086a27af95a643db2356edd6a71c81019672a
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-27 01:05:11 +0530

    Counter Functionality

--- ReactTest/src/App.jsx
+++ ReactTest/src/App.jsx
@@ -1,13 +1,34 @@
 
+import { useState } from 'react';
 import './App.css'
 
 
 function App() {
-  
 
   return (
-    <div>Yo nigga</div>
-  )
+    <div>
+      Yo nigga
+    <Counter />
+    </div>
+  );
+}
+
+
+function Counter() {
+
+  const [count, setcount] = useState(0);
+
+  // let count  = 0;
+  function increasecount(){
+    setcount(count+1);
+  }
+
+  return (
+    <div>
+      <h1>{count}</h1>
+    <button onClick={increasecount}>Increase Count</button>
+    </div>
+  );
 }
 
 export default App
