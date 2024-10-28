5f557283f0c66eb09bde2a316fd68bdda6a1d056
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-29 01:13:28 +0530

    Final

--- useEffect-test/src/App.jsx
+++ useEffect-test/src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
-  const [currentTab, setCurrentTab] = useState("feed");
+  const [currentTab, setCurrentTab] = useState(undefined);
+
+  useEffect(() => {
+    console.log("Current tab is " + currentTab);
+  }, [currentTab])
 
 return (
 <div>
