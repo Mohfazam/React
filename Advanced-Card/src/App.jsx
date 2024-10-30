a889a2a47550881aa6b8ba255205409db3fe2aa5
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-30 20:26:09 +0530

    Enhanced Card Component

--- Advanced-Card/src/App.jsx
+++ Advanced-Card/src/App.jsx
@@ -1,28 +1,32 @@
+import React from 'react';
 
+const Card = ({ children }) => {
+    return (
+        <div style={{
+            border: '1px solid #ccc',
+            borderRadius: '5px',
+            padding: '20px',
+            margin: '10px',
+            boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.1)',
+        }}>
+            {children}
+        </div>
+    );
+};
 
-function App() {
-
-
-  return (
-  <div style={{display: "flex"}}>
-
-    <Card>
-    
-      What do you want to do?
-      <br />
-      <input type="text" />
-    </Card>
-    
-  </div>
-  );
-}
-
-function Card({ innerContent }){
-  return(
-    <span style={{backgroundColor: "blue", borderRadius: 10, color: "white", margin: 10, padding: 10}}>
-      {innerContent}
-    </span>
-  )
-}
+const App = () => {
+    return (
+        <div>
+            <Card>
+                <h2>Card Title</h2>
+                <p>This is some content inside the card.</p>
+            </Card>
+            <Card>
+                <h2>Another Card</h2>
+                <p>This card has different content!</p>
+            </Card>
+        </div>
+    );
+};
 
 export default App;
