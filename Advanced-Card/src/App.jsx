d3526cc2059d880e8e098bd12186154a58368032
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-30 19:52:32 +0530

    Advaned Card

--- Advanced-Card/src/App.jsx
+++ Advanced-Card/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <div>
+    <Card innerContent={"Hi there"} />
+  </div>
+  );
+}
+
+function Card({ innerContent }){
+  return(
+    <span style={{backgroundColor: "black", borderRadius: 10, color: "white", margin: 10, padding: 10}}>
+      {innerContent}
+    </span>
   )
 }
 
-export default App
+export default App;
