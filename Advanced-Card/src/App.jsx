cb5a7a417b0fc8480376eac0262cdfdd71ef39c0
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-30 20:09:23 +0530

    Children

--- Advanced-Card/src/App.jsx
+++ Advanced-Card/src/App.jsx
@@ -5,20 +5,21 @@ function App() {
 
   return (
   <div style={{display: "flex"}}>
-    <Card innerContent={"Hi there"} />
-    <Card innerContent={<div style={{color: "green"}}>Hi there</div>} />
-    <Card innerContent={<div style={{color: "red"}}>
+
+    <Card>
+    
       What do you want to do?
       <br />
       <input type="text" />
-    </div>} />
+    </Card>
+    
   </div>
   );
 }
 
 function Card({ innerContent }){
   return(
-    <span style={{backgroundColor: "black", borderRadius: 10, color: "white", margin: 10, padding: 10}}>
+    <span style={{backgroundColor: "blue", borderRadius: 10, color: "white", margin: 10, padding: 10}}>
       {innerContent}
     </span>
   )
