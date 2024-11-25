0d58527bcec7f301f47b4e93cc8fde06b38a85aa
Mohfazam <mohd.sarwar.code@gmail.com>
2024-11-25 23:34:23 +0530

    Validation

--- useForm/src/App.jsx
+++ useForm/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-  
+
+  function onSubmit(data){
+    console.log("Submitting the form", data);
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label>First Name: </label>
-        <input/>
+        <input {...register('firstName', {required: true,  maxLength: 20})}/>
       </div>
-
+      <br />
       <div>
         <label>Middle Name: </label>
-        <input/>
+        <input {...register('middlename', {required: true,  maxLength: 20})}/>
       </div>
+      <br />
 
       <div>
         <label>last Name: </label>
-        <input/>
+        <input {...register('lastname', {required: true,  maxLength: 20})}/>
       </div>
+
+      <input type="submit" value="Submit" />
     </form>
-  )
+  );
 }
 
-export default App
+export default App;
