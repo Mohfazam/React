76ae40159543a27930bb8f7f42930d1120a4a805
Mohfazam <mohd.sarwar.code@gmail.com>
2024-11-25 23:19:04 +0530

    React Form

--- useForm/src/App.jsx
+++ useForm/src/App.jsx
@@ -1,14 +1,34 @@
 import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 import './App.css'
 
 function App() {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <form>
+      <div>
+        <label>First Name: </label>
+        <input/>
+      </div>
+
+      <div>
+        <label>Middle Name: </label>
+        <input/>
+      </div>
+
+      <div>
+        <label>last Name: </label>
+        <input/>
+      </div>
+    </form>
   )
 }
 
